bf23584f4e94049a43b48534857bd38edf855e4e
Albert Liu <albertliu@naturalwellbeing.com>
2018-07-30 08:34:48 -0700

    use differnt hook

--- app/overrides/decorate_smarty_street_tags.rb
+++ app/overrides/decorate_smarty_street_tags.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(
     :virtual_path => "spree/checkout/_address",
     :name => "smarty_street_tags",
-    :insert_after => "[data-hook='billing_fieldset_wrapper']",
+    :insert_after => "[data-hook='address_footer']",
     :partial => "spree/shared/smarty_street_tags",
     :disabled => false)
